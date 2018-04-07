3074a3bbe7aa765cb07f5279a21085ca08d0954f
Emily Lin <emily@Emilys-MBP.hitronhub.home>
2018-04-07 00:37:28 -0400

    centered social media icons

--- src/aboutMe/AboutMe.js
+++ src/aboutMe/AboutMe.js
@@ -11,7 +11,7 @@ const AboutMe = () => {
                 <p className={classes.title}>About Me</p>
                 <p className={classes.paragraph}>Why hello there! My name is Emily, and I’m currently studying Software Engineering at the University of Waterloo. When I’m not coding, you can find me reading, playing the violin, piano, or guitar, and watching anime and movies! I also enjoy hiking and skiing.</p>
                 <p className={classes.connect}><strong>Connect with me</strong>!</p>
-                <div>
+                <div className={classes.connect}>
                     <a target="_blank" href='https://www.linkedin.com/in/emily-lin-95b636124'>
                         <img className={classes.linkedIn} src={linkedIn} alt="LinkedIn Icon"/>
                     </a>
